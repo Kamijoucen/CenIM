d1eb3e45cba1909e713981d205d08ef7db2ee21c
Kamijoucen <lisicen12138@outlook.com>
2020-07-29 23:09:29 +0800

    init

---
+++ src/main/kotlin/com/kamijoucen/cenim/CenImApplication.kt
@@ -0,0 +1,11 @@
+package com.kamijoucen.cenim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CenImApplication
+
+fun main(args: Array<String>) {
+    runApplication<CenImApplication>(*args)
+}
---
+++ src/test/kotlin/com/kamijoucen/cenim/CenImApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kamijoucen.cenim
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CenImApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
